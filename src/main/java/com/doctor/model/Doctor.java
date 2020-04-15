c42bd210e772730546db727a6b065b5eb9c80cfd
Savandi <savandi98@gmail.com>
2020-04-16 00:47:52 +0530

    Doctor Management REST API creation

---
+++ src/main/java/com/doctor/model/Doctor.java
@@ -0,0 +1,152 @@
+package com.doctor.model;
+
+
+public class Doctor {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String password;
+    private String joinedDate;
+    private int phone;
+    private String specialization;
+    private String address;
+    private String NIC;
+    private String doctor_id;
+    private String hospital_id;
+    private String patient_id;
+    private String appointment_id;
+
+    public String getAppointment_id() {
+        return appointment_id;
+    }
+
+    public void setAppointment_id(String appointment_id) {
+        this.appointment_id = appointment_id;
+    }
+
+    public String getHospital_id() {
+        return hospital_id;
+    }
+
+    public void setHospital_id(String hospital_id) {
+        this.hospital_id = hospital_id;
+    }
+
+    public String getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(String patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(String joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(String doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", joinedDate='" + joinedDate + '\'' +
+                ", phone=" + phone +
+                ", specialization='" + specialization + '\'' +
+                ", address='" + address + '\'' +
+                ", NIC='" + NIC + '\'' +
+                ", doctor_id='" + doctor_id + '\'' +
+                ", hospital_id='" + hospital_id + '\'' +
+                ", patient_id='" + patient_id + '\'' +
+                ", appointment_id='" + appointment_id + '\'' +
+                '}';
+    }
+}
